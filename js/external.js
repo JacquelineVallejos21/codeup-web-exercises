0136c85de76c36bfacb5932ad5e16c6c945e6e2a
Jacqueline Vallejos <84371982+JacquelineVallejos21@users.noreply.github.com>
2021-05-27 19:45:50 -0500

    exercise 3 js still updating

--- js/external.js
+++ js/external.js
@@ -5,26 +5,55 @@ console.log("Hello from external JavaScript");
 
 alert("Welcome to my Website!");
 
-// the following line will show the OK/CANCEL confirm dialog
-var confirmed = confirm("What's your favorite color?");
-console.log(confirmed); // will be either true or false
-
-var userInput = prompt("rose pink");
-console.log("Great, rose pink is my favorite color too!" + userInput);
+var color;
+color = prompt("What's your favorite color?");
+var message;
+message = ("Great," + color + " is my favorite color!");
+alert(message);
 
 //Exercise 3 Answers
+//You have rented some movies for your kids: The little mermaid (for 3 days),
+// Brother Bear (for 5 days, they love it), and Hercules (1 day,
+// you don't know yet if they're going to like it). If price for a movie per day is $3,
+// how much will you have to pay?
 
-alert("The price for the movies is: " + (3+5+1) *3);
+alert("The price for the movies is: " + (3+5+1)* 3);
 
-alert("I am getting paid: " + ((400 * 10) * (300 * 6) * (350 *4)));
+//Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook,
+// they pay you a different rate per hour. Google pays $400, Amazon $380, and Facebook $350.
+// How much will you receive in payment for this week? You worked 10 hours for Facebook,
+// 6 hours for Google and 4 hours for Amazon.
+alert("I am getting paid for this week: " + ((400*6) + (380*4) + (350*10)));
 
-var full = confirm ("Is the class full");
+
+//A student can be enrolled in a class only if the class is not full and
+// the class schedule does not conflict with her current schedule.
+var full = confirm ("Is the class full?");
 var conflict = confirm ("Is there a conflict?");
+alert("You can enroll in class:" + (!full && !conflict));
+
+//A product offer can be applied only if a person buys more than 2 items,
+// and the offer has not expired. Premium members do not need to buy a specific amount of products.
+
+var premium= confirm("Are you a premium member?");
+var quantity= Number (prompt ("How many items did you buy"));
+var expired= confirm ("Is the coupon expired");
+alert("You can get the discount" + ((!expired) && (premium || (quantity > 2))));
+
+//Use the following code to follow the instructions below:
+//
+//
+// var username = 'codeup';
+// var password = 'notastrongpassword';
+// Create a variable that holds a boolean value for each of the following conditions:
+//
+// the password must be at least 5 characters
+// the password must not include the username
+// the username must be no more than 20 characters
+// neither the username or password can start or end with whitespace
 
 
-var premium= confirm
-var quantity= Number
-var expired=
- alert
+
+
 
 
