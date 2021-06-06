41efadd5e5db22685cd20ea22ca6d9f666242c76
Jacqueline Vallejos <84371982+JacquelineVallejos21@users.noreply.github.com>
2021-06-05 20:49:34 -0500

    break and continue js

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,25 +1,5 @@
-//var random= Math.floor((Math.random()*50) +1);
-
-//console.log("Random odd number to skip is: " + random);
-
-//for (var i = 1; i <100; i++) {
-
-//    if (i % 2 === 0) {
-        //Skip the even numbers.
-//        continue;
-//    }
-
-//    if (random === i ) {
-//   console.log("Yikes! Skipping number: " + i);
-//    } else {
-//        console.log("Here is an odd number:" + i);
-//    }
-
-//    if (i >=49) {
-//        break;
-//    }
-var oddNumber
-oddNumber=prompt("what is your number:" + oddNumber)
+var oddNumber;
+oddNumber=prompt("Choose a number between 1 and 50");
 
 for (var index = 0; index < 50; index++) {
 
