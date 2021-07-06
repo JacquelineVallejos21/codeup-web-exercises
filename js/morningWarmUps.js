f49904ca7392a65288ce34efe547455d824c8582
Jacqueline Vallejos <84371982+JacquelineVallejos21@users.noreply.github.com>
2021-07-06 09:30:59 -0500

    morningwarmups

--- js/morningWarmUps.js
+++ js/morningWarmUps.js
@@ -1,4 +1,4 @@
-// TODO: Create a function named countSpaces that accepts a string and returns the total number of spaces in the string.
+// TODO: Cle.log(countSpaces(str));reate a function named countSpaces that accepts a string and returns the total number of spaces in the string.
 const str = 'this is a string';
 const countSpaces = (str = '') => {
     let count = 0;
@@ -12,5 +12,21 @@ const countSpaces = (str = '') => {
 };
 console.log(countSpaces(str));
 
+// July 6th 2021
+// TODO: Create a function named capitalizeAllNames that accepts an array of strings and returns the array with all
+//  of its elements capitalized.
+// Example:  capitalizeAllNames(["bob","Seth","TOFU","Toyota"]) ---- returns ----> ["BOB","SETH","TOFU","TOYOTA"]
+function capitalizeAllNames(x){
+        var array2 = ["BOB","SETH","TOFU","TOYOTA"];
+    array2 = array2.map(function(x){ return x.toUpperCase(); })
+}
 
-// TODO: Add validation to function above, if the argument pass is not a string it should return false.
+// TODO: Create a function named capitalizeFirstLetter that accepts an array of strings and
+//  returns the array with all of the first letters in
+//  each string being capitalized while having the rest of the string lowercase look at the example below for reference.
+// Example:  capitalizeFirstLetter(["bob","Seth","TOFU","Toyota"]) ---- returns ----> ["Bob","Seth","Tofu","Toyota"]
+function capitalizeFirstLetter(x){
+    var array2 = ["Bob","Seth","Tofu","Toyota"];
+    array2 = array2.map(function(x){ return x.toUpperCase(); })
+}
+
