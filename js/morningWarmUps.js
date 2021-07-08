012c7e98a255431ac733400b5b5cfe55d436587d
Jacqueline Vallejos <84371982+JacquelineVallejos21@users.noreply.github.com>
2021-07-08 10:18:44 -0500

    add examples

--- js/morningWarmUps.js
+++ js/morningWarmUps.js
@@ -135,3 +135,29 @@ console.log(getTallUsers(people));
 // Example: getTallUsers(people)
 // ---- returns ----> [{firstName: "silvia", lastName: "floopertan", ageInYears: 34, heightInInches: 65},
 //
+
+
+
+// "use strict";
+// ​
+// var myVar = "apple";
+// ​
+// function foo(param1) {
+//     console.log("in foo, myVar is visible and its value is " + myVar);
+//     console.log("in foo, param1 is visible and its value is " + param1);
+//     console.log("in foo, param2 is NOT visible");
+// }
+// ​
+// function bar(param2) {
+//     param2 = "grape";
+//     console.log("in bar, myVar is visible and its value is " + myVar);
+//     console.log("in bar, param1 is NOT visible");
+//     console.log("in bar, param2 is visible and its value is " + param2);
+// }
+// ​
+// console.log("outside functions, myVar is visible and its value is " + myVar);
+// console.log("outside functions, param1 is NOT visible");
+// console.log("outside functions, param2 is NOT visible");
+// ​
+// foo("banana");
+// bar(myVar);
