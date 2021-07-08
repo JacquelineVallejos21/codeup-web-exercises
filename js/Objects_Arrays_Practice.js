2a8b40fb07588da21ab05066b91fc9346ca50960
Jacqueline Vallejos <84371982+JacquelineVallejos21@users.noreply.github.com>
2021-07-08 17:04:25 -0500

    updating

--- js/Objects_Arrays_Practice.js
+++ js/Objects_Arrays_Practice.js
@@ -12,6 +12,7 @@
 // removeDuplicates(["2016-02-13","1994-12-21","2016-02-13","2016-02-13","2005-11-29"]);
 // // Should return the following
 // ["1994-12-21","2016-02-13","2005-11-29"]
+
 var arr = ["bob","squid","samantha","bob"];
 
 function removeDuplicates(data){
@@ -31,6 +32,23 @@ function removeDuplicates(data){
     return [...new Set(data)]
 }
 console.log(removeDuplicates(arr));
+
+// function removeDuplicates(arrayOfStrings){
+//
+//     var uniqueStrings = [];
+//     for (let i = 0; i < arrayOfStrings.length; i++){
+//         if (uniqueStrings.includes(arrayStrings[i])){
+//
+//         }else{
+//             uniqueStrings.push(arrayOfStrings[i])
+//         }
+//     }
+//     return uniqueStrings
+// }
+//
+// console.log(removeDuplicates(["bob", "squid","samantha", "bob"]));
+// console.log(removeDuplicates(["tilda", "tilda"]));
+// console.log(removeDuplicates(["2016-02-13","1994-12-21","2016-02-13","2016-02-13","2005-11-29"]));
 
 // 2. Create a function called max that accepts an array of numbers and returns the largest number from that array.
 //     Examples
@@ -97,6 +115,35 @@ console.log(max([54.5,2,34,-93]));
 // mostOccuringNumber([73.5,8,0,-3,3.50,8.25,3.50]);
 // // Should return the following
 // 3.5
+//
+
+// function mostOccuringNumber(numbers){
+//     var counts = [];
+//      for (let i = 0; i < number.length; i++){
+//          var aNumber = numbers[i];
+//          if (counts[aNumber]) === undefined) {
+//              counts[aNumber] = 1;
+//          } else {
+//          counts[aNumber] ++; //gives us access to the counts element at index "aNumber"
+//      }
+// }
+//      // console.log(counts);
+//      //2. scan through the associative array and keep track of the value with the highest count
+// var winningCount = 0;
+// var winningNumber = "";
+// for(const countsKey in counts){
+//     console.log("the counts element at index" + countsKey + "is value" + counts[countsKey]);
+//     var currentCount = counts[countsKey];
+//     if(currentCount > maxCount){
+//         //update 2 things when we have a new winner
+//         maxCount =currentCount;
+// }
+// }
+//
+// // console.log(mostOccuringNumber([8,93,28,8,27,-62,32,8,-62]));
+// // console.log(mostOccuringNumber([8,93,-62.0,28,27,-62,32,8,-62]));
+// // console.log(mostOccuringNumber([73.5,8,0,-3,3.50,8.25,3.50]));
+// }
 var arr1=[8,93,28,8,27,-62,32,8,-62];
 var mf = 1;
 var m = 0;
@@ -156,6 +203,7 @@ for (var i=0; i<arr3.length; i++)
     m=0;
 }
 console.log(item+" ( " +mf +" times ) ") ;
+
 
 //4. Create a function called whoIsPassing that accepts an array of objects, where each object is a student's name
 // and class average (from 0 to 100). Your function will return an array of objects where each object is a student's
@@ -226,6 +274,99 @@ console.log(item+" ( " +mf +" times ) ") ;
 // ]
 
 
+function whoIsPassing(arrayOfStudents){
+
+    var passingResults = [];
+
+
+    for (let i = 0; i < arrayOfStudents.length; i++) {
+
+
+        if (arrayOfStudents[i].classAverage >= 60){
+            // this means they are passing
+
+            passingResults.push(
+                {
+                name: arrayOfStudents[i].name,
+                passing: true
+                }
+            )
+
+        }else{
+            // this means the student is failing
+
+            passingResults.push(
+                {
+                    name: arrayOfStudents[i].name,
+                    passing: false
+                }
+            )
+
+        }
+    }
+
+    return passingResults;
+
+}
+
+function whoIsPassing(arrayOfStudents){
+
+
+    var passingResults = [];
+
+
+    for (let i = 0; i < arrayOfStudents.length; i++) {
+
+        var currentStudentsName = arrayOfStudents[i].name
+
+
+        var isCurrentStudentPassing = (arrayOfStudents[i].classAverage >= 60);
+
+        passingResults.push(
+            {
+                name: currentStudentsName,
+                passing: isCurrentStudentPassing
+            }
+        )
+    }
+
+    return passingResults;
+}
+
+var students = [
+    {
+        name: "Phill Rundy",
+        classAverage: 53
+    },
+    {
+        name: "Samuel Moore",
+        classAverage: 71
+    },
+    {
+        name: "Mark Tendly",
+        classAverage: 84
+    },
+    {
+        name: "Dough Mosh",
+        classAverage: 87
+    },
+    {
+        name: "Bran Thomb",
+        classAverage: 93
+    },
+    {
+        name: "Mario Yushi",
+        classAverage: 82
+    },
+    {
+        name: "Nathan Skywalker",
+        classAverage: 52
+    }
+]
+                    // arrayOfStudents = students
+console.log(whoIsPassing(students));
+
+
 // 5. Create a function called dateStringToObject that accepts a date in the following string format "YYYY-MM-DD" and returns an object that contains month, day, and year properties.
 //     Examples
 // dateStringToObject("2016-2-13");
@@ -239,6 +380,16 @@ console.log(item+" ( " +mf +" times ) ") ;
 // dateStringToObject("1994-12-21");
 // // Should return the following
 // {month: 12, day: 21, year: 1994}
+
+function dateStringToObject(string) {
+
+    var splitString=string.split("-")
+    return {month: splitString[1], day: splitString[2], year: splitString[0]};
+}
+console.log(dateStringToObject("2016-2-13"));
+console.log(dateStringToObject("2005-11-29"));
+console.log(dateStringToObject("1994-12-21"));
+
 
 
 
@@ -255,6 +406,7 @@ console.log(item+" ( " +mf +" times ) ") ;
 // reverseString("2016-02-13");
 // // Should return the following
 // "31-21-6102"
+
 function string_reverse(str)
     {
    return str.split("").reverse().join("");
@@ -263,6 +415,17 @@ function string_reverse(str)
 console.log(string_reverse("squid"));
 console.log(string_reverse("tilda"));
 console.log(string_reverse("2016-02-13"));
+
+// function reverseString(string){
+//
+//     var revString = ""
+//    for (let i = string.length - 1; i >=0 ; i--){
+//        revString += string.charAt(i);
+//     }
+//    return revString
+// }
+
+
 
 // 7. Create a function called numberInfo that accepts a number and returns an object with the properties isPositive, isEven, isZero. The properties should either be true or false based on if the parameter meets the criteria.
 //     Examples
@@ -289,3 +452,29 @@ console.log(string_reverse("2016-02-13"));
 //         isEven: true,
 //     isZero: true
 // }
+
+
+function numberInfo(number) {
+
+    var isNumberPositive = (number > 0);
+    var isNumberEven = (number % 2 === 0);
+    var isNumberZero = (number === 0);
+
+    return {
+        isPositive: isNumberPositive,
+        isEven: isNumberEven,
+        isZero: isNumberZero,
+    }
+}
+
+console.log(numberInfo(-1));
+console.log(numberInfo(6));
+console.log(numberInfo(0));
+
+// function numberInfo(number){
+//         return {
+//         isPositive: number > 0;
+//         isEven: number % 2 === 0;
+//         isZero: number === 0;
+// }
+
