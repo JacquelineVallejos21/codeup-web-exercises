d3a529d52d290a367cc873f872a155391cc65a72
Jacqueline Vallejos <84371982+JacquelineVallejos21@users.noreply.github.com>
2021-07-09 17:45:23 -0500

    j query lecture

--- js/externalQuery.js
+++ js/externalQuery.js
@@ -1,18 +1,40 @@
 "use strict"
+var h1;
+var aDiv
+$(document).ready(function (){
 
-$(document).ready(function() {
+    h1 = $("h1")
 
-    var h1 = $("h1")
+    console.log(h1.html("Sam was here"))
 
-    console.log(h1)
+    alert("hey I am ready for manipulation!")
 
-alert("hey I am ready")
 
-    var aDiv = $(".content")
+    aDiv = $("#something")
+
+    console.log(aDiv)
+
+    var divs = $(".content")
 
     console.log(divs)
 
+    divs.addClass("imgStuff")
+
+    var stuff = $(".content, h1")
+
+    console.log(stuff)
+
     var image = $("img")
 
+    // image.css("width","50%")
+    // image.css("border","solid 5px red")
+
+    image.css({"width":"50%","border":"solid 5px red"})
+
+    console.log("The images width is " + image.css("width"))
+
+    // console.log(image.css())
+
+    $("div > p,h1").css({"background-color":"cyan"})
 
 })
